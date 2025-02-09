3e47bf22f5305510bc7461368764e415205eee69
akmallxx <akmaloktavian111@gmail.com>
2025-02-10 06:51:31 +0700

    update landing page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,9 +4,9 @@
         <form action="" class="mb-12">
             <div class="relative">
                 <input type="text" name="search" placeholder="Cari film..."
-                    class="w-full px-4 py-3 text-black border-gray-300 bg-gray-100 rounded-sm focus:ring-0 focus:border-red-500 dark:bg-neutral-950 dark:text-white dark:border-black dark:focus:border-red-500">
+                    class="w-full px-4 py-3 text-black border-gray-300 bg-gray-100 rounded-md focus:ring-0 focus:border-red-500 dark:bg-neutral-950 dark:text-white dark:border-black dark:focus:border-red-500">
                 <button type="submit"
-                    class="text-gray-400 dark:text-white absolute inset-y-0 right-0 flex items-center px-4 rounded-e-sm">
+                    class="text-gray-400 dark:text-white absolute inset-y-0 right-0 flex items-center px-4 rounded-e-md">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M21 21l-4.35-4.35m0 0a7.5 7.5 0 1 0-1.5 1.5L21 21z" />
@@ -15,136 +15,140 @@
             </div>
         </form>
 
-        <div id="trend">
-            <div class="border-l-4 border-red-600">
-                <h2 class="ms-2 text-black dark:text-white font-semibold text-xl">SEDANG TREN</h2>
+        <!-- Start movies Page -->
+        <div id="movies" class="my-8 md:my-16">
+            <div class="flex justify-between">
+                <h2 class="text-black dark:text-white font-semibold text-lg md:text-xl">MOVIES</h2>
+                <a href="movies">
+                    <span class="bg-red-800 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm hover:text-gray-400 dark:text-white">Lihat Semua</span>
+                </a>
             </div>
-            <!-- Swiper -->
-            <div class="mt-4 pembatas swiper mySwiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide rounded-md">
+            <div class="position-relative border-b border-gray-400">
+                <hr class="thick-hr">
+            </div>
+            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
 
-                        <div class="relative max-w-sm">
-                            <a href="detail" class="">
-                                <div class="w-full">
-                                    <img class="w-full h-auto aspect-[3/4] object-cover transition-transform duration-300 group-hover:scale-105"
-                                        src="https://asset.tix.id/wp-content/uploads/2025/01/f3e9f805-8c13-4cb5-a50e-815d26b92712-600x885.webp"
-                                        alt="COMPANION" />
-                                </div>
-                                <div class=" dark:text-white bg-neutral-100 dark:bg-neutral-900 p-1">
-                                    <h5 class="text-sm font-bold drop-shadow">Companion</h5>
-                                    <p class="text-xs dark:text-gray-300">
-                                        2024
-                                    </p>
-                                </div>
-                            </a>
+                <div class="relative max-w-sm overflow-hidden group">
+                    <a href="detail" class="block">
+                        <div class="w-full relative">
+                            <img class="w-full h-auto aspect-[3/4] object-cover transition-transform duration-300 group-hover:scale-105"
+                                src="https://asset.tix.id/wp-content/uploads/2025/01/f3e9f805-8c13-4cb5-a50e-815d26b92712-600x885.webp"
+                                alt="COMPANION" />
                         </div>
-                    </div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    <div class="swiper-slide">Slide 4</div>
-                    <div class="swiper-slide">Slide 5</div>
-                    <div class="swiper-slide">Slide 6</div>
-                    <div class="swiper-slide">Slide 7</div>
-                    <div class="swiper-slide">Slide 8</div>
-                    <div class="swiper-slide">Slide 9</div>
+                        <div class="dark:text-white bg-neutral-100 dark:bg-neutral-900 p-1">
+                            <h5 class="text-sm font-bold drop-shadow">Companion</h5>
+                            <p class="text-xs dark:text-gray-300">
+                                2024
+                            </p>
+                        </div>
+                    </a>
                 </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
+
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+                <div class="swiper-slide">Slide 4</div>
+                <div class="swiper-slide">Slide 5</div>
+                <div class="swiper-slide">Slide 6</div>
+                <div class="swiper-slide">Slide 7</div>
+                <div class="swiper-slide">Slide 8</div>
+                <div class="swiper-slide">Slide 9</div>
+                <div class="swiper-slide">Slide 10</div>
+                <div class="swiper-slide">Slide 11</div>
+                <div class="swiper-slide">Slide 12</div>
+
             </div>
         </div>
-        <div id="latest">
-            <div class="border-l-4 border-red-600">
-                <h2 class="ms-2 text-black dark:text-white font-semibold text-xl">TERBARU</h2>
-            </div>
-            <!-- Swiper -->
-            <div class="mt-4 pembatas swiper mySwiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide rounded-md">
+        <!-- End movies Page -->
 
-                        <div class="relative max-w-sm overflow-hidden group shadow-lg">
-                            <a href="detail" class="block">
-                                <div class="w-full relative">
-                                    <img class="w-full h-auto aspect-[3/4] object-cover transition-transform duration-300 group-hover:scale-105"
-                                        src="https://asset.tix.id/wp-content/uploads/2025/01/f3e9f805-8c13-4cb5-a50e-815d26b92712-600x885.webp"
-                                        alt="COMPANION" />
-                                </div>
-                            </a>
-                            <div
-                                class="absolute bottom-0 p-2 text-white bg-gradient-to-t from-black/80 via-black/80 to-transparent w-full">
-                                <h5 class="text-sm font-bold drop-shadow">Companion</h5>
-                                <p class="text-xs text-gray-300">
-                                    2024
-                                </p>
-                                <p class="text-xs pt-2">
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star text-gray-400"></span>
-                                </p>
-                            </div>
+        <!-- Start series page -->
+        <div id="series" class="my-8 md:my-16">
+            <div class="flex justify-between">
+                <h2 class="text-black dark:text-white font-semibold text-lg md:text-xl">SERIES</h2>
+                <a href="series">
+                    <span class="bg-red-800 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm hover:text-gray-400 dark:text-white">Lihat Semua</span>
+                </a>
+            </div>
+            <div class="position-relative border-b border-gray-400">
+                <hr class="thick-hr">
+            </div>
+            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
+
+                <div class="relative max-w-sm overflow-hidden group">
+                    <a href="detail" class="block">
+                        <div class="w-full relative">
+                            <img class="w-full h-auto aspect-[3/4] object-cover transition-transform duration-300 group-hover:scale-105"
+                                src="https://asset.tix.id/wp-content/uploads/2025/01/f3e9f805-8c13-4cb5-a50e-815d26b92712-600x885.webp"
+                                alt="COMPANION" />
                         </div>
-                    </div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    <div class="swiper-slide">Slide 4</div>
-                    <div class="swiper-slide">Slide 5</div>
-                    <div class="swiper-slide">Slide 6</div>
-                    <div class="swiper-slide">Slide 7</div>
-                    <div class="swiper-slide">Slide 8</div>
-                    <div class="swiper-slide">Slide 9</div>
+                        <div class="dark:text-white bg-neutral-100 dark:bg-neutral-900 p-1">
+                            <h5 class="text-sm font-bold drop-shadow">Companion</h5>
+                            <p class="text-xs dark:text-gray-300">
+                                2024
+                            </p>
+                        </div>
+                    </a>
                 </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
+
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+                <div class="swiper-slide">Slide 4</div>
+                <div class="swiper-slide">Slide 5</div>
+                <div class="swiper-slide">Slide 6</div>
+                <div class="swiper-slide">Slide 7</div>
+                <div class="swiper-slide">Slide 8</div>
+                <div class="swiper-slide">Slide 9</div>
+                <div class="swiper-slide">Slide 10</div>
+                <div class="swiper-slide">Slide 11</div>
+                <div class="swiper-slide">Slide 12</div>
+
             </div>
         </div>
-        <div id="anime">
-            <div class="border-l-4 border-red-600">
-                <h2 class="ms-2 text-black dark:text-white font-semibold text-xl">ANIME</h2>
-            </div>
-            <!-- Swiper -->
-            <div class="mt-4 pembatas swiper mySwiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide rounded-md">
+        <!-- End series page -->
 
-                        <div class="relative max-w-sm overflow-hidden group shadow-lg">
-                            <a href="detail" class="block">
-                                <div class="w-full relative">
-                                    <img class="w-full h-auto aspect-[3/4] object-cover transition-transform duration-300 group-hover:scale-105"
-                                        src="https://asset.tix.id/wp-content/uploads/2025/01/f3e9f805-8c13-4cb5-a50e-815d26b92712-600x885.webp"
-                                        alt="COMPANION" />
-                                </div>
-                            </a>
-                            <div
-                                class="absolute bottom-0 p-2 text-white bg-gradient-to-t from-black/80 via-black/80 to-transparent w-full">
-                                <h5 class="text-sm font-bold drop-shadow">Companion</h5>
-                                <p class="text-xs text-gray-300">
-                                    2024
-                                </p>
-                                <p class="text-xs pt-2">
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star checked text-yellow-400"></span>
-                                    <span class="fa fa-star text-gray-400"></span>
-                                </p>
-                            </div>
+        <!-- Start anime Page -->
+        <div id="anime" class="my-8 md:my-16">
+            <div class="flex justify-between">
+                <h2 class="text-black dark:text-white font-semibold text-lg md:text-xl">ANIME</h2>
+                <a href="anime">
+                    <span class="bg-red-800 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm hover:text-gray-400 dark:text-white">Lihat Semua</span>
+                </a>
+            </div>
+            <div class="position-relative border-b border-gray-400">
+                <hr class="thick-hr">
+            </div>
+            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
+
+                <div class="relative max-w-sm overflow-hidden group">
+                    <a href="detail" class="block">
+                        <div class="w-full relative">
+                            <img class="w-full h-auto aspect-[3/4] object-cover transition-transform duration-300 group-hover:scale-105"
+                                src="https://asset.tix.id/wp-content/uploads/2025/01/f3e9f805-8c13-4cb5-a50e-815d26b92712-600x885.webp"
+                                alt="COMPANION" />
                         </div>
-                    </div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    <div class="swiper-slide">Slide 4</div>
-                    <div class="swiper-slide">Slide 5</div>
-                    <div class="swiper-slide">Slide 6</div>
-                    <div class="swiper-slide">Slide 7</div>
-                    <div class="swiper-slide">Slide 8</div>
-                    <div class="swiper-slide">Slide 9</div>
+                        <div class="dark:text-white bg-neutral-100 dark:bg-neutral-900 p-1">
+                            <h5 class="text-sm font-bold drop-shadow">Companion</h5>
+                            <p class="text-xs dark:text-gray-300">
+                                2024
+                            </p>
+                        </div>
+                    </a>
                 </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
+
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+                <div class="swiper-slide">Slide 4</div>
+                <div class="swiper-slide">Slide 5</div>
+                <div class="swiper-slide">Slide 6</div>
+                <div class="swiper-slide">Slide 7</div>
+                <div class="swiper-slide">Slide 8</div>
+                <div class="swiper-slide">Slide 9</div>
+                <div class="swiper-slide">Slide 10</div>
+                <div class="swiper-slide">Slide 11</div>
+                <div class="swiper-slide">Slide 12</div>
+
             </div>
         </div>
+        <!-- End anime Page -->
 
     </div>
 
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,2 +1,92 @@
 <x-app-layout>
+
+    <div class="p-12">
+        <form action="" class="mb-12">
+            <div class="relative">
+                <input type="text" name="search" placeholder="Cari film..."
+                    class="w-full px-4 py-3 text-black border-gray-300 bg-neutral-200 rounded-md focus:ring-0 focus:border-red-500 dark:bg-neutral-950 dark:text-white dark:border-black dark:focus:border-red-500">
+                <button type="submit"
+                    class="text-gray-400 dark:text-white absolute inset-y-0 right-0 flex items-center px-4 rounded-e-md">
+                    <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M21 21l-4.35-4.35m0 0a7.5 7.5 0 1 0-1.5 1.5L21 21z" />
+                    </svg>
+                </button>
+            </div>
+        </form>
+
+        <div class="border-red-600 flex justify-between">
+            <h2 class="ms-2 text-black dark:text-white font-semibold text-xl">MOVIES</h2>
+        </div>
+        <div class="position-relative border-b border-gray-400">
+            <hr class="thick-hr">
+        </div>
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
+
+            <div class="relative max-w-sm overflow-hidden group">
+                <a href="detail" class="block">
+                    <div class="w-full relative">
+                        <img class="w-full h-auto aspect-[3/4] object-cover transition-transform duration-300 group-hover:scale-105"
+                            src="https://asset.tix.id/wp-content/uploads/2025/01/f3e9f805-8c13-4cb5-a50e-815d26b92712-600x885.webp"
+                            alt="COMPANION" />
+                    </div>
+                    <div class="dark:text-white bg-neutral-100 dark:bg-neutral-900 p-1">
+                        <h5 class="text-sm font-bold drop-shadow">Companion</h5>
+                        <p class="text-xs dark:text-gray-300">
+                            2024
+                        </p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+            <div class="swiper-slide">Slide 4</div>
+            <div class="swiper-slide">Slide 5</div>
+            <div class="swiper-slide">Slide 6</div>
+            <div class="swiper-slide">Slide 7</div>
+            <div class="swiper-slide">Slide 8</div>
+            <div class="swiper-slide">Slide 9</div>
+            <div class="swiper-slide">Slide 10</div>
+            <div class="swiper-slide">Slide 11</div>
+            <div class="swiper-slide">Slide 12</div>
+
+        </div>
+        <nav aria-label="Page navigation example">
+            <ul class="flex items-center -space-x-px h-10 text-base justify-center mt-6">
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-neutral-500 bg-white border border-e-0 border-neutral-300 rounded-s-lg hover:bg-neutral-100 hover:text-neutral-700 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-white">
+                        <span class="sr-only">Previous</span>
+                        <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-neutral-500 bg-white border border-neutral-300 hover:bg-neutral-100 hover:text-neutral-700 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-white">1</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-neutral-500 bg-white border border-neutral-300 hover:bg-neutral-100 hover:text-neutral-700 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-white">2</a>
+                </li>
+                <li>
+                    <a href="#" aria-current="page" class="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-neutral-700 dark:bg-neutral-700 dark:text-white">3</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-neutral-500 bg-white border border-neutral-300 hover:bg-neutral-100 hover:text-neutral-700 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-white">4</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-neutral-500 bg-white border border-neutral-300 hover:bg-neutral-100 hover:text-neutral-700 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-white">5</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-neutral-500 bg-white border border-neutral-300 rounded-e-lg hover:bg-neutral-100 hover:text-neutral-700 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-white">
+                        <span class="sr-only">Next</span>
+                        <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
 </x-app-layout>
